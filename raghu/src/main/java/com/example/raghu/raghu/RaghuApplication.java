826ea29f1f24d0a906ffaa7abe82b30344d8e332
ubantu <ubantu@ubantu-Lenovo-IdeaPad-S145-15IKB>
2022-07-21 15:03:48 +0530

    cc

---
+++ raghu/src/main/java/com/example/raghu/raghu/RaghuApplication.java
@@ -0,0 +1,13 @@
+package com.example.raghu.raghu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaghuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaghuApplication.class, args);
+	}
+
+}
---
+++ raghu/src/test/java/com/example/raghu/raghu/RaghuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raghu.raghu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaghuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
